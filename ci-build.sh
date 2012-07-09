c73d34c2cd8e278aa16b046158c18030fa629d22
Erik Bernoth <erik.bernoth@googlemail.com>
2012-07-09 17:17:09 +0200

    write nightowl files
    Signed-off-by: Erik Bernoth <erik.bernoth@googlemail.com>

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo "Hello World"
+PRE=nightowl
+ERROR_LOG=$PRE-error.log
+ERROR_GRAPH=$PRE-error
+
+cat $1/log | ./filter_errorlog.py >>$ERROR_LOG
+cat $ERROR_LOG | ./to_json.py | grep count | ./to_graph.py $ERROR_GRAPH
