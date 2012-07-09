99e4b943c8ca49c7306b57d81e7c6ca30caca774
Erik Bernoth <erik.bernoth@googlemail.com>
2012-07-09 17:29:28 +0200

    testing2
    Signed-off-by: Erik Bernoth <erik.bernoth@googlemail.com>

--- ci-build.sh
+++ ci-build.sh
@@ -4,6 +4,5 @@ PRE=nightowl
 ERROR_LOG=$PRE-error.log
 ERROR_GRAPH=$PRE-error
 
-echo $1
-#cat $1/log | ./filter_errorlog.py >>$ERROR_LOG
-#cat $ERROR_LOG | ./to_json.py | grep count | ./to_graph.py $ERROR_GRAPH
+cat $1/log | ./filter_errorlog.py >>$ERROR_LOG
+cat $ERROR_LOG | ./to_json.py | grep count | ./to_graph.py $ERROR_GRAPH
