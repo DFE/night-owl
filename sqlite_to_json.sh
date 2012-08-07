e320cd85b4ce6c284a34d69c243998666e2c0bf0
Erik Bernoth <erik.bernoth@googlemail.com>
2012-08-07 13:56:38 +0200

    database: replaced quotation marks
        Python's json framework doesn't like single quotes to mark strings, thus
        all the `'` was replaced with `"` everywhere.
        close #15
    
    Signed-off-by: Erik Bernoth <erik.bernoth@googlemail.com>

--- sqlite_to_json.sh
+++ sqlite_to_json.sh
@@ -2,18 +2,18 @@
 
 #FIXME: This is all too much hackety hack. Rewrite!
 json1="{"
-json1="$json1 'tid':\1,"
-json1="$json1 'type':'\2',"
-json1="$json1 'attempt':\3,"
-json1="$json1 'msg':'\4',"
-json1="$json1 'cat':'\5',"
-json1="$json1 'recipe':'\6',"
-json1="$json1 'cmd':'\7',"
-json1="$json1 'file':'\8',"
+json1="$json1 \"tid\":\1,"
+json1="$json1 \"type\":\"\2\","
+json1="$json1 \"attempt\":\3,"
+json1="$json1 \"msg\":\"\4\","
+json1="$json1 \"cat\":\"\5\","
+json1="$json1 \"recipe\":\"\6\","
+json1="$json1 \"cmd\":\"\7\","
+json1="$json1 \"file\":\"\8\","
 #deadline hackety hack, because sed just allows until \9
-json2="\1 'row':\2,"
-json2="$json2 'col':\3,"
-json2="$json2 'time':'\4'"
+json2="\1 \"row\":\2,"
+json2="$json2 \"col\":\3,"
+json2="$json2 \"time\":\"\4\""
 json2="$json2 }"
 
 e="\([^|]*\)"
@@ -28,4 +28,5 @@ fi
 
 sqlite3 $1 "$sql" \
     | sed "s/$e|$e|$e|$e|$e|$e|$e|$e\(.*\)/$json1\9/" \
-    | sed "s/$e|$e|$e|$e/$json2/"
+    | sed "s/$e|$e|$e|$e/$json2/" \
+#    | sed "s/'/\\\"/g"
